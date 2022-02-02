6318554c7cfa49b368613d69defaa3244551b172
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-02-02 13:47:43 +0700

    :tada: First commit

---
+++ awesome/themes/zz/gmc.lua
@@ -0,0 +1,36 @@
+local _M = {}
+
+-- Associative Array (Hash)
+
+_M.color = {
+  ['white'] = '#ffffff',
+  ['black'] = '#000000',
+  ['gray50'] = '#fafafa',
+  ['gray100'] = '#f5f5f5',
+  ['gray200'] = '#eeeeee',
+  ['gray300'] = '#e0e0e0',
+  ['gray400'] = '#bdbdbd',
+  ['gray500'] = '#9e9e9e',
+  ['gray600'] = '#757575',
+  ['gray700'] = '#616161',
+  ['gray800'] = '#424242',
+  ['gray900'] = '#212121',
+  ['red50'] = '#ffebee',
+  ['red100'] = '#ffcdd2',
+  ['red200'] = '#ef9a9a',
+  ['red300'] = '#e57373',
+  ['red400'] = '#ef5350',
+  ['red500'] = '#f44336',
+  ['red600'] = '#e53935',
+  ['red700'] = '#d32f2f',
+  ['red800'] = '#c62828',
+  ['red900'] = '#b71c1c',
+  ['red900dark'] = '#7f0000',
+  ['redA100'] = '#ff8a80',
+  ['redA200'] = '#ff5252',
+  ['redA400'] = '#ff1744',
+  ['redA700'] = '#d50000',
+  ['redA700dark'] = '#9b0000',
+}
+
+return _M
