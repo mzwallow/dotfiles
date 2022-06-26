5334fffdd1675bc9ca97b0a6ac74ef1ba3f44682
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-06-26 20:15:46 +0700

    :hammer: Update setup script

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,18 +4,18 @@ sudo pacman -S git go neovim vi vim nano zsh ttf-jetbrains-mono wget \
   noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk \
   htop docker docker-compose pipewire pipewire-alsa pipewire-jack \
   pipewire-pulse pavucontrol alsa-firmware sof-firmware alsa-ucm-conf \
-  tree kitty --noconfirm
+  tree kitty
 
-sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+#sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
 cd $HOME
-mkdir Packages
+#mkdir Packages
 cd "Packages"
 git clone https://aur.archlinux.org/yay.git
 cd "yay"
 makepkg -sirc
 
-yay -S google-chrome --noconfirm
+yay -S google-chrome
 
 chsh -s /bin/zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
