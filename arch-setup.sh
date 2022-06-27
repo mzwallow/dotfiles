0d91f3fe0ce35847e0b41b54e9d1173a708122ea
Punnarat Rattanapawan <punnarat@hotmail.com>
2022-06-27 23:06:51 +0700

    :hammer: Add setxkbmap to .xinitrc, Set awesome to defualt, Add packages to arch-setup.sh

---
+++ arch-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo pacman -S go zsh ttf-jetbrains-mono wget \
+  noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk \
+  htop docker docker-compose pipewire pipewire-alsa pipewire-jack \
+  pipewire-pulse pavucontrol alsa-firmware sof-firmware alsa-ucm-conf \
+  tree kitty picom pipewire-v4l2 gstreamer gst-plugin-pipewire xorg xorg-xinit \
+  nvidia nvidia-settings nvidia-prime mesa mesa-utils xf86-video-intel vulkan-intel \
+  discord ttf-th-sarabun-new fonts-tlwg nm-connection-editor network-manager-applet \
+  openvpn networkmanager-openvpn jre17-openjdk dbeaver seahorse --noconfirm
+
+cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
+ln -s $HOME/Packages/dotfiles/awesome $HOME/.config/awesome
+ln -s $HOME/Packages/dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+
+yay -S google-chrome postman-bin visual-studio-code-bin --noconfirm
+
+chsh -s /bin/zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# plugins=(
+#   git
+#   archlinux
+#   zsh-autosuggestions
+#   zsh-syntax-highlighting
+#   zsh-completions
+#   zsh-history-substring-search
+# )
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# ZSH_THEME="spaceship"
