f51a55b89cf03f5d15cfbfc871a6c69dc3aefbdb
Punnarat Rattanapawan <punnarat@hotmail.com>
2022-06-27 11:25:22 +0700

    :hammer: Add packages

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-sudo pacman -S git go neovim vi vim nano zsh ttf-jetbrains-mono wget \
+sudo pacman -S zsh ttf-jetbrains-mono wget \
   noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk \
   htop docker docker-compose pipewire pipewire-alsa pipewire-jack \
   pipewire-pulse pavucontrol alsa-firmware sof-firmware alsa-ucm-conf \
-  tree kitty picom pipewire-v4l2 gstreamer gst-plugin-pipewire --noconfirm
+  tree kitty picom pipewire-v4l2 gstreamer gst-plugin-pipewire xorg xorg-xinit \
+  nvidia nvidia-settings nvidia-prime mesa mesa-utils xf86-video-intel vulkan-intel \
+  discord ttf-th-sarabun-new fonts-tlwg --noconfirm
 
-#sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
+ln -s $HOME/Packages/dotfiles/awesome $HOME/.config/awesome
 
 cd $HOME
-#mkdir Packages
-cd "Packages"
-git clone https://aur.archlinux.org/yay.git
-cd "yay"
-makepkg -sirc
+#cd "Packages"
+#git clone https://aur.archlinux.org/yay.git
+#cd "yay"
+#makepkg -sirc
 
 yay -S google-chrome
 
