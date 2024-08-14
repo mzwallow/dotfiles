5dd10a75edae33d4e18f647796e158741ac2c1b1
Punnarat Rattanapawan <punnarat.r@numeiang.com>
2024-08-14 13:36:25 +0700

    fix(nvim): remove omitempty tag for go

--- config/nvim.custom/lua/mzwallow/plugins/lsp.lua
+++ config/nvim.custom/lua/mzwallow/plugins/lsp.lua
@@ -308,7 +308,9 @@ return {
 	{
 		"ray-x/go.nvim",
 		config = function()
-			require("go").setup()
+			require("go").setup({
+				tag_options = '',
+			})
 		end,
 		event = { "CmdlineEnter" },
 		ft = { "go", "gomod" },
