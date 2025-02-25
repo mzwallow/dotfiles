031dd83465ed8919297f9adab0808563b33852b9
mzwallow <punnarat_mos@hotmail.com>
2025-02-25 23:43:16 +0700

    feat(win): add static ip script

---
+++ psh-static-ip.bat
@@ -0,0 +1,24 @@
+$IP = "192.168.1.11"
+$MaskBits = 24 # This means subnet mask = 255.255.255.0
+$Gateway = "192.168.1.1"
+$Dns = "192.168.1.250"
+$IPType = "IPv4"
+# Retrieve the network adapter that you want to configure
+$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+ $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+ # Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+ -AddressFamily $IPType `
+ -IPAddress $IP `
+ -PrefixLength $MaskBits `
+ -DefaultGateway $Gateway
+# Configure the DNS client server IP addresses
+$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
+# Disable IPv6
+$adapter | Disable-NetAdapterBinding -ComponentID ms_tcpip6
