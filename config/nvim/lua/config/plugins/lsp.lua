f8c1a05b75500a98ca7d840c83258e48e3b7da1a
mzwallow <punnarat_mos@hotmail.com>
2025-03-09 21:14:54 +0700

    feat(nvim): rewriting nvim config

---
+++ config/nvim/lua/config/plugins/lsp.lua
@@ -0,0 +1,89 @@
+local servers = {
+	"lua_ls",
+	"gopls",
+}
+
+local tools = {
+	"golangci-lint",
+}
+
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{
+				"saghen/blink.cmp",
+				dependencies = "rafamadriz/friendly-snippets",
+				version = "*",
+				opts = {
+					sources = {
+						-- add lazydev to your completion providers
+						default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+						providers = {
+							lazydev = {
+								name = "LazyDev",
+								module = "lazydev.integrations.blink",
+								-- make lazydev completions top priority (see `:h blink.cmp`)
+								score_offset = 100,
+							},
+						},
+					},
+					signature = {
+						enabled = true,
+						trigger = {
+							show_on_keyword = true,
+							show_on_insert = false,
+						},
+					}
+				},
+			}
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+			require("mason-tool-installer").setup({
+				ensure_installed = vim.tbl_deep_extend("force", servers, tools),
+				auto_update = true,
+			})
+
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({ capabilities = capabilities })
+				end
+			})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local c = vim.lsp.get_client_by_id(args.data.client_id)
+					if not c then
+						return
+					end
+
+					-- Format the current buffer on save
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+						end,
+					})
+				end,
+			})
+		end,
+	},
+}
---
+++ config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		-- Optional dependencies
+		dependencies = { "echasnovski/mini.icons", opts = {} },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		config = function()
+			require("oil").setup({
+				watch_for_changes = true,
+				win_options = {
+					signcolumn = "yes:2",
+				},
+				view_options = { show_hidden = true },
+			})
+			vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+		end
+	},
+}
