40e713c6336eab74317447d240c63886d5b94fa0
Punnarat Rattanapawan <punnarat.r@numeiang.com>
2025-03-22 20:14:36 +0700

    feat(nvim): re-config oil keymaps

--- config/nvim/lua/config/plugins/oil.lua
+++ config/nvim/lua/config/plugins/oil.lua
@@ -5,10 +5,29 @@ return {
 		dependencies = { "echasnovski/mini.icons", opts = {} },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 		config = function()
+			local detail = false
+
 			require("oil").setup({
-				watch_for_changes = true,
 				win_options = {
-					signcolumn = "yes:2",
+					signcolumn = "no",
+				},
+				watch_for_changes = true,
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-j>"] = false,
+					["<C-k>"] = false,
+					["<C-l>"] = false,
+					["gd"] = {
+						desc = "Toggle file detail view",
+						callback = function()
+							detail = not detail
+							if detail then
+								require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+							else
+								require("oil").set_columns({ "icon" })
+							end
+						end,
+					},
 				},
 				view_options = { show_hidden = true },
 			})
