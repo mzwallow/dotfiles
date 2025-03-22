7df01739db77b74871a92c7aa62a27f8483dc5dd
mzwallow <punnarat_mos@hotmail.com>
2025-03-22 20:04:31 +0700

    feat(debian): add direnv

--- debian-init.sh
+++ debian-init.sh
@@ -2,6 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt-get install -y build-essential curl wget zsh tmux yq fzf zip unzip ripgrep fd-find gcc
+sudo apt-get install -y direnv
 sudo apt-get install -y pkg-config libssl-dev # Required for avante.nvim
 sudo apt-get install -y lua5.4 lua5.1 luarocks
 sudo apt-get install -y python3 python3-pip python3-venv
