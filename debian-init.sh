40f914940051bf8e0abb060171b442afad1c17d9
mzwallow <punnarat_mos@hotmail.com>
2025-03-23 12:34:44 +0700

    refactor(debian): rewrite config

--- debian-init.sh
+++ debian-init.sh
@@ -1,45 +1,42 @@
 #!/bin/sh
+
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install -y build-essential curl wget zsh tmux yq fzf zip unzip ripgrep fd-find gcc
+sudo apt-get install -y build-essential curl wget zsh yq fzf zip unzip ripgrep fd-find gcc
 sudo apt-get install -y direnv
 sudo apt-get install -y pkg-config libssl-dev # Required for avante.nvim
 sudo apt-get install -y lua5.4 lua5.1 luarocks
 sudo apt-get install -y python3 python3-pip python3-venv
 sudo apt-get install -y gh
 
-curl https://mise.run | sh
 curl https://sh.rustup.rs -sSf | sh
-. "$HOME/.cargo/env"
 
-mkdir -p $HOME/.config
-ln -sf $PWD/config/nvim $HOME/.config/nvim
-ln -sf $PWD/config/tmux $HOME/.config/tmux
-ln -sf $PWD/config/starship.toml $HOME/.config/starship.toml
+mkdir -p "$HOME/.config"
 
-sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" && \
-	ln -sf $PWD/config/zshrc-debian $HOME/.zshrc
+echo "Installing tmux..."
+sudo apt-get install -y tmux && \
+    ln -sfn "$PWD/config/tmux" "$HOME/.config/tmux"
 
-# Installing Go
-GO_VERSION=$(git ls-remote --tags --sort=-version:refname https://github.com/golang/go | grep -o -m 1 -E "(go[1-9]+\.[0-9]+\.[0-9]+)$")
-GO_DL="${GO_VERSION}.linux-amd64.tar.gz"
-curl -LO "https://go.dev/dl/${GO_DL}" && \
-	sudo rm -rf /usr/local/go && sudo tar -xzvf $GO_DL -C /usr/local && \
-	rm $GO_DL && \ 
-	echo "$(go version)"
+echo "Installing mise..."
+curl https://mise.run | sh && \
+    ln -sfn "$PWD"/config/mise "$HOME"/.config/mise && \
+    mise install
 
-# Installing Neovim
-echo "Installing Neovim..."
+
+echo "Installing ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+    ln -sfn "$PWD/config/zshrc-debian" "$HOME/.zshrc" && \
+    . "$HOME/.zshrc"
+
+
+echo "Installing starship..."
+curl -sS https://starship.rs/install.sh | sh && \
+    ln -sfn "$PWD"/config/starship.toml "$HOME"/.config/starship.toml
+
+
+echo "Installing neovim..."
 sudo mkdir -p /opt/nvim
 sudo curl -Lo /opt/nvim/nvim "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage" && \
-	sudo chown $USER:$USER /opt/nvim/nvim && sudo chmod u+x /opt/nvim/nvim
-
-
-
-echo "
-Run following commands manually...
-
-    $ sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"
-    $ curl -sS https://starship.rs/install.sh | sh
-
-"
+    sudo chown "$USER":"$USER" /opt/nvim/nvim && \
+    sudo chmod u+x /opt/nvim/nvim && \
+    ln -sfn "$PWD/config/nvim" "$HOME/.config/nvim"
