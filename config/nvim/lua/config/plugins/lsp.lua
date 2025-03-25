c448e1e1da4e98980ad8ef4a9155017d6adaf527
Punnarat Rattanapawan <punnarat.r@numeiang.com>
2025-03-25 16:05:10 +0700

    chore(nvim): update packages

--- config/nvim/lua/config/plugins/lsp.lua
+++ config/nvim/lua/config/plugins/lsp.lua
@@ -77,8 +77,7 @@ return {
 											name = "openapi3_0",
 											description = "OpenAPI 3.0.X",
 											fileMatch = { "**/*.openapi3_0.yml", "**/*.openapi3_0.yaml" },
-											url =
-											"https://raw.githubusercontent.com/stoplightio/spectral/refs/heads/develop/packages/rulesets/src/oas/schemas/oas/v3.0.json",
+											url = "https://raw.githubusercontent.com/stoplightio/spectral/refs/heads/develop/packages/rulesets/src/oas/schemas/oas/v3.0.json",
 										},
 									},
 								}),
