8e0b844bdcf247684d2794fd70bb27ce38b62f9f
mzwallow <punnarat_mos@hotmail.com>
2025-04-02 20:00:46 +0700

    fix(win11): correct dynamic ip interface script

--- psh-dynamic-ip.bat
+++ psh-dynamic-ip.bat
@@ -1,6 +1,6 @@
 $IPType = "IPv4"
 # Change '? {$_.Status -eq "up"}' to Wi-Fi
-$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+$adapter = Get-NetAdapter | ? {$_.Name -eq "Wi-Fi"}
 $interface = $adapter | Get-NetIPInterface -AddressFamily $IPType
-$interface | Set-NetIPInterface -InterfaceAlias $adapter -Dhcp Enabled
+$interface | Set-NetIPInterface -Dhcp Enabled
 $interface | Set-DnsClientServerAddress -ResetServerAddresses
