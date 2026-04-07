76b456a8da3c892e062928e0b58c2f03944ff4ec
MZwallow <punnarat_mos@hotmail.com>
2026-04-07 23:17:54 +0700

    feat: add tpope vim plugins and update tmux config

---
+++ config/nvim.new/lua/plugins/tpope.lua
@@ -0,0 +1,9 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "tpope/vim-sensible",
+  "tpope/vim-repeat",
+  "tpope/vim-surround",
+  "tpope/vim-commentary",
+  { "tpope/vim-obsession", dependencies = { "tpope/vim-pathogen" } },
+}
