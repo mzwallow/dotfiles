5747aff890cb06e7f94aab85dc2fc71a4069759e
Punnarat Rattanapawan <punnarat.r@numeiang.com>
2026-05-05 00:45:33 +0700

    feat(nvim): add svelte formatter, ftplugin, and fix tailwindcss lsp name

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,208 +1,208 @@
 local servers = {
-	"lua_ls",
-	"gopls",
-	"pyright",
-	"ts_ls",
-	"clangd",
-	-- "zls",
-	-- Terraform
-	"terraformls",
-	-- Assembly
-	"asm-lsp",
-	-- Web
-	"html",
-	"cssls",
-	"tailwindcss",
-	"svelte",
-	"vue_ls",
-	-- "vtsls",
-	"wgsl_analyzer",
+  "lua_ls",
+  "gopls",
+  "pyright",
+  "ts_ls",
+  "clangd",
+  -- "zls",
+  -- Terraform
+  "terraformls",
+  -- Assembly
+  "asm-lsp",
+  -- Web
+  "html",
+  "cssls",
+  "tailwindcss-language-server",
+  "svelte",
+  "vue_ls",
+  -- "vtsls",
+  "wgsl_analyzer",
 
-	"yamlls",
-	"spectral",
-	"jsonls",
-	"bashls",
-	"tombi",
+  "yamlls",
+  "spectral",
+  "jsonls",
+  "bashls",
+  "tombi",
 
-	"hyprls",
+  "hyprls",
 }
 
 local tools = {
-	-- Lua
-	"stylua",
-	-- Go
-	"golangci-lint",
-	"revive",
-	"golines",
-	"goimports-reviser",
-	"gci",
-	"gofumpt",
-	"djlint",
-	-- Python
-	"ruff",
-	-- TS & JS
-	"htmlhint",
-	"stylelint",
-	"eslint_d",
-	"oxlint",
-	-- C/C++
-	"cpptools",
-	"cortex-debug",
-	"cpplint",
-	"clang-format",
-	-- Terraform
-	"terraform",
-	"tflint",
-	-- Assembly
-	"asmfmt",
+  -- Lua
+  "stylua",
+  -- Go
+  "golangci-lint",
+  "revive",
+  "golines",
+  "goimports-reviser",
+  "gci",
+  "gofumpt",
+  "djlint",
+  -- Python
+  "ruff",
+  -- TS & JS
+  "htmlhint",
+  "stylelint",
+  "eslint_d",
+  "oxlint",
+  -- C/C++
+  "cpptools",
+  "cortex-debug",
+  "cpplint",
+  "clang-format",
+  -- Terraform
+  "terraform",
+  "tflint",
+  -- Assembly
+  "asmfmt",
 
-	"yamllint",
-	"jsonlint",
-	"prettierd",
-	-- Bash
-	"shellcheck",
+  "yamllint",
+  "jsonlint",
+  "prettierd",
+  -- Bash
+  "shellcheck",
 }
 
 local ensure_installed = servers
 vim.list_extend(ensure_installed, tools)
 
 return {
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = {
-			{
-				"mason-org/mason-lspconfig.nvim",
-				opts = {},
-				dependencies = {
-					{ "mason-org/mason.nvim", opts = {} },
-					"neovim/nvim-lspconfig",
-				},
-			},
-			"b0o/schemastore.nvim",
-		},
-		config = function()
-			-- require("mason").setup()
-			-- require("mason-lspconfig").setup()
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+          { "mason-org/mason.nvim", opts = {} },
+          "neovim/nvim-lspconfig",
+        },
+      },
+      "b0o/schemastore.nvim",
+    },
+    config = function()
+      -- require("mason").setup()
+      -- require("mason-lspconfig").setup()
 
-			require("mason-tool-installer").setup({
-				ensure_installed = ensure_installed,
-				auto_update = true,
-			})
+      require("mason-tool-installer").setup({
+        ensure_installed = ensure_installed,
+        auto_update = true,
+      })
 
-			vim.lsp.config("yamlls", {
-				settings = {
-					yaml = {
-						format = {
-							enable = true,
-							bracketSpacing = true,
-						},
-						validate = true,
-						hover = true,
-						completion = true,
-						schemaStore = {
-							enable = false,
-							url = "",
-						},
-						schemas = require("schemastore").yaml.schemas({
-							extra = {
-								{
-									name = "openapi3_0",
-									description = "OpenAPI 3.0.X",
-									fileMatch = { "**/*.openapi3_0.yml", "**/*.openapi3_0.yaml" },
-									url = "https://raw.githubusercontent.com/stoplightio/spectral/refs/heads/develop/packages/rulesets/src/oas/schemas/oas/v3.0.json",
-								},
-							},
-						}),
-					},
-				},
-			})
+      vim.lsp.config("yamlls", {
+        settings = {
+          yaml = {
+            format = {
+              enable = true,
+              bracketSpacing = true,
+            },
+            validate = true,
+            hover = true,
+            completion = true,
+            schemaStore = {
+              enable = false,
+              url = "",
+            },
+            schemas = require("schemastore").yaml.schemas({
+              extra = {
+                {
+                  name = "openapi3_0",
+                  description = "OpenAPI 3.0.X",
+                  fileMatch = { "**/*.openapi3_0.yml", "**/*.openapi3_0.yaml" },
+                  url = "https://raw.githubusercontent.com/stoplightio/spectral/refs/heads/develop/packages/rulesets/src/oas/schemas/oas/v3.0.json",
+                },
+              },
+            }),
+          },
+        },
+      })
 
-			vim.lsp.config("jsonls", {
-				settings = {
-					json = {
-						schemas = require("schemastore").json.schemas(),
-						validate = { enable = true },
-					},
-				},
-			})
+      vim.lsp.config("jsonls", {
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
 
-			-- Vue
-			-- local vue_language_server_path = vim.fn.expand("$MASON/packages")
-			-- 	.. "/vue-language-server"
-			-- 	.. "/node_modules/@vue/language-server"
-			-- local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-			-- local vue_plugin = {
-			-- 	name = "@vue/typescript-plugin",
-			-- 	location = vue_language_server_path,
-			-- 	languages = { "vue" },
-			-- 	configNamespace = "typescript",
-			-- }
-			-- local vtsls_config = {
-			-- 	settings = {
-			-- 		vtsls = {
-			-- 			tsserver = {
-			-- 				globalPlugins = {
-			-- 					vue_plugin,
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	},
-			-- 	filetypes = tsserver_filetypes,
-			-- }
-			-- local vue_ls_config = {}
-			-- vim.lsp.config("vtsls", vtsls_config)
-			-- vim.lsp.config("vue_ls", vue_ls_config)
-			-- vim.lsp.enable({ "ts_ls", "vue_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
-			--
-			-- -- TypeScript
-			-- local ts_ls_config = {
-			-- 	init_options = {
-			-- 		plugins = {
-			-- 			vue_plugin,
-			-- 		},
-			-- 	},
-			-- 	filetypes = tsserver_filetypes,
-			-- }
-			-- vim.lsp.config("ts_ls", ts_ls_config)
+      -- Vue
+      -- local vue_language_server_path = vim.fn.expand("$MASON/packages")
+      -- 	.. "/vue-language-server"
+      -- 	.. "/node_modules/@vue/language-server"
+      -- local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+      -- local vue_plugin = {
+      -- 	name = "@vue/typescript-plugin",
+      -- 	location = vue_language_server_path,
+      -- 	languages = { "vue" },
+      -- 	configNamespace = "typescript",
+      -- }
+      -- local vtsls_config = {
+      -- 	settings = {
+      -- 		vtsls = {
+      -- 			tsserver = {
+      -- 				globalPlugins = {
+      -- 					vue_plugin,
+      -- 				},
+      -- 			},
+      -- 		},
+      -- 	},
+      -- 	filetypes = tsserver_filetypes,
+      -- }
+      -- local vue_ls_config = {}
+      -- vim.lsp.config("vtsls", vtsls_config)
+      -- vim.lsp.config("vue_ls", vue_ls_config)
+      -- vim.lsp.enable({ "ts_ls", "vue_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+      --
+      -- -- TypeScript
+      -- local ts_ls_config = {
+      -- 	init_options = {
+      -- 		plugins = {
+      -- 			vue_plugin,
+      -- 		},
+      -- 	},
+      -- 	filetypes = tsserver_filetypes,
+      -- }
+      -- vim.lsp.config("ts_ls", ts_ls_config)
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-				callback = function(args)
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					if not client then
-						return
-					end
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
 
-					-- Highlight references word under cursor
-					if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+          -- Highlight references word under cursor
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
 
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = args.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = args.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
 
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = args.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = args.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
 
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-							callback = function(args2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = args2.buf })
-							end,
-						})
-					end
-				end,
-			})
+            vim.api.nvim_create_autocmd("LspDetach", {
+              group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+              callback = function(args2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = args2.buf })
+              end,
+            })
+          end
+        end,
+      })
 
-			local telescope_builtin = require("telescope.builtin")
-			vim.keymap.set("n", "grr", telescope_builtin.lsp_references)
-			vim.keymap.set("n", "gri", telescope_builtin.lsp_implementations)
-		end,
-	},
+      local telescope_builtin = require("telescope.builtin")
+      vim.keymap.set("n", "grr", telescope_builtin.lsp_references)
+      vim.keymap.set("n", "gri", telescope_builtin.lsp_implementations)
+    end,
+  },
 }
